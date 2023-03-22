c2f6321883437a3e177a18195e8135783c1b04fd
valerRien <valeracedin@yandex.ru>
2023-03-22 11:15:08 +0300

    Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.

---
+++ src/main/java/org/example/miniprojects/codewars/StringRepeat.java
@@ -0,0 +1,7 @@
+package org.example.miniprojects.codewars;
+
+public class StringRepeat {
+    public static String repeatStr(final int repeat, final String string) {
+        return string.repeat(repeat);
+    }
+}
