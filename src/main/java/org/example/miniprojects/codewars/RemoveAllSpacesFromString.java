1061a220768fe2f68044a2f451555a6de73ebd91
valerRien <valeracedin@yandex.ru>
2023-03-22 11:12:44 +0300

    Write a function that removes the spaces from the string, then return the resultant string.

---
+++ src/main/java/org/example/miniprojects/codewars/RemoveAllSpacesFromString.java
@@ -0,0 +1,7 @@
+package org.example.miniprojects.codewars;
+
+public class RemoveAllSpacesFromString {
+    public static String noSpace(final String x) {
+        return x.replaceAll(" ", "");
+    }
+}
