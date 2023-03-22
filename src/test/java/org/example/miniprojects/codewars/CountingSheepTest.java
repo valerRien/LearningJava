663acacf4063ff407f8c1a0a8349f88fb50f2de8
valerRien <valeracedin@yandex.ru>
2023-03-22 11:27:40 +0300

    test for kata

---
+++ src/test/java/org/example/miniprojects/codewars/CountingSheepTest.java
@@ -0,0 +1,20 @@
+package org.example.miniprojects.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountingSheepTest {
+
+    Boolean[] array1 = {true,  true,  true,  false,
+            true,  true,  true,  true ,
+            true,  false, true,  false,
+            true,  false, false, true ,
+            true,  true,  true,  true ,
+            false, false, true,  true };
+
+    @Test
+    public void test() {
+        assertEquals("There are 17 sheeps in total", 17, CountingSheep.countSheeps(array1));
+    }
+}
