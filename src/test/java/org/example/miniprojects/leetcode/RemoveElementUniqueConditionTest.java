7a0474dab3443e499561360f9e6777bfb0e9f42e
valerRien <valeracedin@yandex.ru>
2023-04-01 15:04:15 +0300

    test

--- src/test/java/org/example/miniprojects/leetcode/RemoveElementUniqueConditionTest.java
+++ src/test/java/org/example/miniprojects/leetcode/RemoveElementUniqueConditionTest.java
@@ -10,6 +10,22 @@ import static org.junit.Assert.*;
  * 0 <= nums.length <= 100
  * 0 <= nums[i] <= 50
  * 0 <= val <= 100
+ *
+ * Custom Judge:
+ *
+ * The judge will test your solution with the following code:
+ *
+ * int[] nums = [...]; // Input array
+ * int val = ...; // Value to remove
+ * int[] expectedNums = [...]; // The expected answer with correct length.
+ *                             // It is sorted with no values equaling val.
+ *
+ * int k = removeElement(nums, val); // Calls your implementation
+ *
+ * assert k == expectedNums.length;
+ * sort(nums, 0, k); // Sort the first k elements of nums
+ * for (int i = 0; i < actualLength; i++) {
+ *     assert nums[i] == expectedNums[i];
  */
 
 public class RemoveElementUniqueConditionTest {
