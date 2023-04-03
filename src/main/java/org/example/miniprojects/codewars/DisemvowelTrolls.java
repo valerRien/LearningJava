7604a61a3868ecf3a47d13894bcedc09b382e461
valerRien <valeracedin@yandex.ru>
2023-04-03 11:03:54 +0300

    Trolls are attacking your comment section!
    A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
    
    Your task is to write a function that takes a string and return a new string with all vowels removed.
    
    For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
    
    Note: for this kata y isn't considered a vowel.

---
+++ src/main/java/org/example/miniprojects/codewars/DisemvowelTrolls.java
@@ -0,0 +1,7 @@
+package org.example.miniprojects.codewars;
+
+public class DisemvowelTrolls {
+    public static String disemvowel(String str) {
+        return str.replaceAll("[AaEeIiOoUu]", "");
+    }
+}
