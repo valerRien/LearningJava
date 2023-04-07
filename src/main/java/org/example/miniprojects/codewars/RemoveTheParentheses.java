8807c4b50d9fdc095306a8e8ee65f53e079e844f
valerRien <valeracedin@yandex.ru>
2023-04-07 12:18:09 +0300

    regular expression here

--- src/main/java/org/example/miniprojects/codewars/RemoveTheParentheses.java
+++ src/main/java/org/example/miniprojects/codewars/RemoveTheParentheses.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class RemoveTheParentheses {
     public static String removeParentheses(final String str) {
-        StringBuilder editingString = new StringBuilder();
+     /*   StringBuilder editingString = new StringBuilder();
         int bracketCounter = 0;
         for (int i = 0; i < str.length(); i++) {
             char currentChar = str.charAt(i);
@@ -20,5 +20,11 @@ public class RemoveTheParentheses {
             }
         }
         return editingString.toString();
+        */
+        String editingString = str;
+        while (editingString.contains("(")) {
+           editingString = editingString.replaceAll("\\([^()]*\\)", "");
+        }
+        return editingString;
     }
 }
