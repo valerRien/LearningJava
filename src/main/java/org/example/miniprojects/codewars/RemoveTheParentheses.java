f6911450d1aa8fdc4019a8d43dd1e0ff5b7a10c5
valerRien <valeracedin@yandex.ru>
2023-04-07 11:57:18 +0300

    refactor

--- src/main/java/org/example/miniprojects/codewars/RemoveTheParentheses.java
+++ src/main/java/org/example/miniprojects/codewars/RemoveTheParentheses.java
@@ -5,33 +5,20 @@ import java.util.List;
 
 public class RemoveTheParentheses {
     public static String removeParentheses(final String str) {
-        List<String> toCutStrings = new ArrayList<>();
-        int bracketCounter = 0, startIndex = 0, endIndex = 0;
+        StringBuilder editingString = new StringBuilder();
+        int bracketCounter = 0;
         for (int i = 0; i < str.length(); i++) {
             char currentChar = str.charAt(i);
             if (currentChar == '(') {
-                startIndex = i;
-                bracketCounter ++;
-                while (bracketCounter != 0 && i < str.length()) {
-                    i++;
-                    currentChar = str.charAt(i);
-                    if (currentChar == ')') {
-                        bracketCounter--;
-                        if (bracketCounter == 0) {
-                            endIndex = i + 1;
-                            toCutStrings.add(str.substring(startIndex, endIndex));
-                        }
-                    } else if (currentChar == '(') {
-                        bracketCounter++;
-                    }
-
-                }
+                bracketCounter++;
+            }
+            if (bracketCounter == 0) {
+                editingString.append(currentChar);
+            }
+            if (currentChar == ')') {
+                bracketCounter--;
             }
         }
-        String editingStr = str;
-        for (String s:toCutStrings) {
-            editingStr = editingStr.replace(s, "");
-        }
-        return editingStr;
+        return editingString.toString();
     }
 }
