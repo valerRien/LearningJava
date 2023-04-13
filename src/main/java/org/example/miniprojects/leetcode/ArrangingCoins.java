88fd90ec387344f3bffdee108c042e506b3b2078
valerRien <valeracedin@yandex.ru>
2023-04-13 14:04:39 +0300

    You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
    Given the integer n, return the number of complete rows of the staircase you will build.

---
+++ src/main/java/org/example/miniprojects/leetcode/ArrangingCoins.java
@@ -0,0 +1,12 @@
+package org.example.miniprojects.leetcode;
+
+public class ArrangingCoins {
+    public static int arrangeCoins(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            n -= i;
+            count++;
+        }
+        return count;
+    }
+}
