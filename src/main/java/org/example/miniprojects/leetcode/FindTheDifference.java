382b7bf7b15a93d68f8331a24db775c71ff7d9f3
valerRien <valeracedin@yandex.ru>
2023-04-13 12:03:43 +0300

    XOR refactor

--- src/main/java/org/example/miniprojects/leetcode/FindTheDifference.java
+++ src/main/java/org/example/miniprojects/leetcode/FindTheDifference.java
@@ -2,10 +2,9 @@ package org.example.miniprojects.leetcode;
 
 public class FindTheDifference {
     public static char findTheDifference(String s, String t) {
-        char[] charsFromS = s.toCharArray();
-        for (int i = 0; i < charsFromS.length; i++) {
-            t = t.replaceFirst(String.valueOf(charsFromS[i]), "");
-        }
-        return t.charAt(0);
+        char result = 0;
+        for (char charS : s.toCharArray()) result = (char) (result ^ charS);
+        for (char charT : t.toCharArray()) result = (char) (result ^ charT);
+        return result;
     }
 }
