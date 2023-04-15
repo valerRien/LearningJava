44ca51440bfb08011b309c1799caeddf93474d24
valerRien <valeracedin@yandex.ru>
2023-04-15 21:09:55 +0300

    test

--- src/test/java/org/example/miniprojects/leetcode/SelfDividingNumbersTest.java
+++ src/test/java/org/example/miniprojects/leetcode/SelfDividingNumbersTest.java
@@ -10,4 +10,9 @@ public class SelfDividingNumbersTest {
     public void sampleTest() {
         assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]", SelfDividingNumbers.selfDividingNumbers(1, 22).toString());
     }
+
+    @Test
+    public void sampleTest2() {
+        assertEquals("[48, 55, 66, 77]", SelfDividingNumbers.selfDividingNumbers(47, 85).toString());
+    }
 }
