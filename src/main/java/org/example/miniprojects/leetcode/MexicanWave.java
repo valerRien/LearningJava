bdda6dafc3144a752097303a1d1cb2974743116e
valerRien <valeracedin@yandex.ru>
2023-04-21 16:01:09 +0300

    In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.  1.  The input string will always be lower case but maybe empty.
     2.  If the character in the string is whitespace then pass over it as if it was an empty seat

---
+++ src/main/java/org/example/miniprojects/leetcode/MexicanWave.java
@@ -0,0 +1,24 @@
+package org.example.miniprojects.leetcode;
+
+import java.util.*;
+
+public class MexicanWave {
+    public static String[] wave(String str) {
+        List<String> resultList = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            if (currentChar >= 97 && currentChar <= 122) {
+                currentChar -= 32;
+                sb.append(str, 0, i);
+                sb.append(currentChar);
+                if (i + 1 < str.length()) {
+                    sb.append(str.substring(i + 1));
+                }
+                resultList.add(sb.toString());
+                sb.setLength(0);
+            }
+        }
+        return resultList.toArray(new String[0]);
+    }
+}
