73e8542dea2f792b902c0681cf43b630f5a5096f
valerRien <valeracedin@yandex.ru>
2023-04-26 15:50:23 +0300

    You are a robot.
    As a robot, you are programmed to take walks following a path.
    
    The path is input to you as a string of the following characters where:
    
    "^" -> step up
    "v" -> step down
    ">" -> step right
    "<" -> step left

---
+++ src/main/java/org/example/miniprojects/codewars/RobotTranslatingAPath.java
@@ -0,0 +1,46 @@
+package org.example.miniprojects.codewars;
+
+public class RobotTranslatingAPath {
+    public static String walk(String path) {
+        if (path.isEmpty()) {
+            return "Paused";
+        }
+        StringBuilder result = new StringBuilder();
+        char previousChar = path.charAt(0);
+        int rowCharsCounter = 0;
+        for (int i = 0; i < path.length(); i++) {
+            char currentChar = path.charAt(i);
+            while (i < path.length() && path.charAt(i) == currentChar) {
+                rowCharsCounter++;
+                i++;
+            }
+            i--;
+
+            result.append(getNavigation(currentChar, rowCharsCounter)).append("\n");
+            rowCharsCounter = 0;
+        }
+
+        return result.toString().trim();
+    }
+
+    private static String getNavigation(char c, int rowCharsCounter) {
+        StringBuilder builder = new StringBuilder();
+        String direction = "";
+        switch (c) {
+            case '^':
+                direction = "up";
+                break;
+            case '>':
+                direction = "right";
+                break;
+            case '<':
+                direction = "left";
+                break;
+            case 'v':
+                direction = "down";
+                break;
+        }
+        builder.append("Take ").append(rowCharsCounter).append(rowCharsCounter > 1 ? " steps " : " step ").append(direction);
+        return builder.toString();
+    }
+}
