39a89a11512bb22be8ac9e5e5e0e151912a6a3e9
valerRien <valeracedin@yandex.ru>
2023-04-27 20:59:18 +0300

    test

---
+++ src/test/java/org/example/miniprojects/leetcode/SubtractTheProductAndSumTest.java
@@ -0,0 +1,14 @@
+package org.example.miniprojects.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubtractTheProductAndSumTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(15, SubtractTheProductAndSum.subtractProductAndSum(234));
+        assertEquals(21, SubtractTheProductAndSum.subtractProductAndSum(4421));
+    }
+}
