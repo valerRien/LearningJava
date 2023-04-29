6c9d7d468c04ea13770ac7faf2fcecea9d45e38d
valerRien <valeracedin@yandex.ru>
2023-04-29 19:01:25 +0300

    test

---
+++ src/test/java/org/example/miniprojects/leetcode/MaximumNumberTest.java
@@ -0,0 +1,15 @@
+package org.example.miniprojects.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaximumNumberTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(9969, MaximumNumber.maximum69Number(9669));
+        assertEquals(9999, MaximumNumber.maximum69Number(9996));
+    }
+
+}
