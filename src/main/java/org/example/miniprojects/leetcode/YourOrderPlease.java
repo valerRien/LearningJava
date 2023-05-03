996efe0e0c955f946d6e66cb306c32f4aacc4775
valerRien <valeracedin@yandex.ru>
2023-05-03 16:24:06 +0300

    Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
    Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
    
    If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.

---
+++ src/main/java/org/example/miniprojects/leetcode/YourOrderPlease.java
@@ -0,0 +1,24 @@
+package org.example.miniprojects.leetcode;
+
+public class YourOrderPlease {
+    public static String order(String words) {
+        if (words == null || words.isEmpty()) return "";
+
+        String[] split = words.split(" ");
+        String[] result = new String[split.length];
+        for (String word : split) {
+            for (int j = 0; j < word.length(); j++) {
+                if (word.charAt(j) > '0' && word.charAt(j) <= '9') {
+                    result[word.charAt(j) - '0' - 1] = word;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String word : result) {
+            sb.append(word).append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+}
