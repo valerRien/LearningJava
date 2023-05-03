e4c570ea6f0db3103ec16b9c971be44ef97cc957
valerRien <valeracedin@yandex.ru>
2023-05-03 16:12:26 +0300

    test

---
+++ src/test/java/org/example/miniprojects/leetcode/BackspacesInStringTest.java
@@ -0,0 +1,18 @@
+package org.example.miniprojects.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BackspacesInStringTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals("ac", BackspacesInString.cleanString("abc#D##c"));
+        assertEquals("a", BackspacesInString.cleanString("abc#D##c#"));
+        assertEquals("aebf", BackspacesInString.cleanString("aec#D#c#bc#f"));
+        assertEquals("", BackspacesInString.cleanString("abc##d######"));
+        assertEquals("", BackspacesInString.cleanString("#######"));
+        assertEquals("", BackspacesInString.cleanString(""));
+    }
+}
