5560cf0bb9e70f9add763e958176273b4b447aec
valerRien <valeracedin@yandex.ru>
2023-05-03 16:24:17 +0300

    test

---
+++ src/test/java/org/example/miniprojects/leetcode/YourOrderPleaseTest.java
@@ -0,0 +1,15 @@
+package org.example.miniprojects.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class YourOrderPleaseTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals("Thi1s is2 3a T4est", YourOrderPlease.order("is2 Thi1s T4est 3a"));
+        assertEquals("Fo1r the2 g3ood 4of th5e pe6ople", YourOrderPlease.order("4of Fo1r pe6ople g3ood th5e the2"));
+        assertEquals("", YourOrderPlease.order(""));
+    }
+}
