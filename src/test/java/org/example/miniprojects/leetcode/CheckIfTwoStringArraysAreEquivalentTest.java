22771c8974a36de61ff09c1eee28dd49c72aba4e
valerRien <valeracedin@yandex.ru>
2023-05-16 15:28:55 +0300

    test

---
+++ src/test/java/org/example/miniprojects/leetcode/CheckIfTwoStringArraysAreEquivalentTest.java
@@ -0,0 +1,14 @@
+package org.example.miniprojects.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CheckIfTwoStringArraysAreEquivalentTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue(CheckIfTwoStringArraysAreEquivalent.arrayStringsAreEqual(new String[]{"ab", "c"}, new String[]{"abc"}));
+    }
+
+}
