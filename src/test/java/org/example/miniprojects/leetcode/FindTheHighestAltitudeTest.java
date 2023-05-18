e979abdcae9757c316119f4e08550ff017b90659
valerRien <valeracedin@yandex.ru>
2023-05-18 22:29:39 +0300

    test

---
+++ src/test/java/org/example/miniprojects/leetcode/FindTheHighestAltitudeTest.java
@@ -0,0 +1,14 @@
+package org.example.miniprojects.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindTheHighestAltitudeTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(1, FindTheHighestAltitude.largestAltitude(new int[]{-5, 1, 5, 0, -7}));
+    }
+
+}
