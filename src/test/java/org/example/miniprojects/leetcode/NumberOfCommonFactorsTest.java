e59918530554ba0981060d5584faf2075b017921
valerRien <valeracedin@yandex.ru>
2023-06-06 16:44:58 +0300

    added throw exception test

--- src/test/java/org/example/miniprojects/leetcode/NumberOfCommonFactorsTest.java
+++ src/test/java/org/example/miniprojects/leetcode/NumberOfCommonFactorsTest.java
@@ -2,6 +2,7 @@ package org.example.miniprojects.leetcode;
 
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,6 +12,13 @@ public class NumberOfCommonFactorsTest {
     public void sampleTest() {
         assertEquals(1, NumberOfCommonFactors.commonFactors(1, 1));
         assertEquals(2, NumberOfCommonFactors.commonFactors(2, 4));
+    }
+
+    @Test
+    public void zeroesTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+           NumberOfCommonFactors.commonFactors(0, 0);
+        });
     }
 
 }
