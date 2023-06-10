9f29e482139d7c69febe3ecbd576258028ad2e33
valerRien <valeracedin@yandex.ru>
2023-06-10 19:54:09 +0300

    test

---
+++ src/test/java/org/example/miniprojects/leetcode/SmallestEven/SmallestEvenMultipleTest.java
@@ -0,0 +1,22 @@
+package org.example.miniprojects.leetcode.SmallestEven;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SmallestEvenMultipleTest {
+
+    @Test
+    void sampleTest() {
+        Assertions.assertEquals(10, SmallestEvenMultiple.smallestEvenMultiple(5));
+    }
+
+    @Test
+    void smallestEvenMultiple() {
+        Assertions.assertEquals(6, SmallestEvenMultiple.smallestEvenMultiple(6));
+    }
+
+    @Test
+    void zeroTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> SmallestEvenMultiple.smallestEvenMultiple(0));
+    }
+}
